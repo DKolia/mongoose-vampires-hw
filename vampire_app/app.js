bc41fe825321ba2268cc671f7a7d04e335268364
David Kolia <Dkolia@icloud.com>
2018-07-18 17:48:20 -0500

    Set up formatting for easy referencing

--- vampire_app/app.js
+++ vampire_app/app.js
@@ -64,37 +64,105 @@ for (var i = 0; i < vampireName.length; i++) {
   console.log("Craeted spooky vampire!");
 }
 
-
+// Now that I have created them.... ???
 
 
 /////////////////////////////////////////////////
 // ## QUERYING
 /////////////////////////////////////////////////
 // ### Select by comparison
+// Write a different query for each of the following:
+// - Find all the vampires that that are females
+// - have greater than 500 victims
+// - have fewer than or equal to 150 victims
+// - have a victim count is not equal to 210234
+// - have greater than 150 AND fewer than 500 victims
+
+
+
+
 
 /////////////////////////////////////////////////
 // ### Select by exists or does not exist
+// Select All Vampires That...
+// - have a title property
+// - do not have a victims property
+// - have a title AND no victims
+// - have victims AND the victims they have are greater than 1000
+
+
+
+
 
 /////////////////////////////////////////////////
 // ### Select with OR
+// Select all the vampires that:
+// - are from New York, New York, US or New Orleans, Louisiana, US
+// - love brooding or being tragic
+// - have more than 1000 victims or love marshmallows
+// - have red hair or green eyes
+
+
+
+
 
 /////////////////////////////////////////////////
 //### Select objects that match one of several values
+// Select all the vampires that:
+// - love either frilly shirtsleeves or frilly collars
+// - love brooding
+// - love at least one of the following: appearing innocent, trickery, lurking in rotting mansions, R&B music
+// - love fancy cloaks but not if they also love either top hats or virgin blood * Hint-You will also have to use $nin *
+
+
+
+
 
 /////////////////////////////////////////////////
 //### Negative Selection
+// Select all vampires that:
+// - love ribbons but do not have brown eyes
+// - are not from Rome
+// - do not love any of the following: [fancy cloaks, frilly shirtsleeves, appearing innocent, being tragic, brooding]
+// - have not killed more than 200 people
+
+
+
+
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // ## REPLACE
+// - replace the vampire called 'Claudia' with a vampire called 'Eve'. 'Eve' will have a key called 'portrayed_by' with the value 'Tilda Swinton'
+// - replace the first male vampire with another whose name is 'Guy Man', and who has a key 'is_actually' with the value 'were-lizard'
+
+
+
+
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // ## UPDATE
+// Update 'Guy Man' to have a gender of 'f'
+// - Update 'Eve' to have a gender of 'm'
+// - Update 'Guy Man' to have an array called 'hates' that includes 'clothes' and 'jobs'
+// - Update 'Guy Man's' hates array also to include 'alarm clocks' and 'jackalopes'
+// - Rename 'Eve's' name field to 'moniker'
+// - We now no longer want to categorize female gender as "f", but rather as fems. Update all females so that the they are of gender "fems".
+
+
+
+
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // ## REMOVE
+// - Remove a single document wherein the hair_color is 'brown'
+// - We found out that the vampires with the blue eyes were just fakes! Let's remove all the vampires who have blue eyes from our database.
+
+
+
+
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
